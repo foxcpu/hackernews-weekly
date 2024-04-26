fb3e787ebd7a32a251abe9a64815182f935f59fd
foxcpu <hulicpu@126.com>
2024-04-26 15:41:25 +0800

    Update issue.js

--- utils/issue.js
+++ utils/issue.js
@@ -17,7 +17,7 @@ const octokit = new Octokit({
     clientId: "Iv1.8d2f7d117f535668",
     clientSecret: process.env.clientSecret ? process.env.clientSecret : secrets.clientSecret,
     // privateKey: process.env.privateKey ? process.env.privateKey : secrets.privateKey,
-    privateKey: "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",
+    privateKey: "11ABCJMOI0HPODpAuxGWtL_pf3ScEZIZ2eDFtDaroSN4Ke2RephOVr9XbTLzMd1znuVTCXEXRUcoVSRGeq",
   },
 });
 
